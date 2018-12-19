420ff4ee2d09c3c0dbd256c534c7f45dddc9435a
Marcin <bialkowski.marcin91@gmail.com>
2018-12-19 14:32:10 +0100

    Add Main Panel

--- Task - ConsoleApp/Program.cs
+++ Task - ConsoleApp/Program.cs
@@ -6,7 +6,23 @@ namespace Task___ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //MAIN PANEL
+            MainPanel();
         }
+
+        // Main Panel View
+        public static void MainPanel()
+        {
+            Console.WriteLine("Choose the method number:");
+            Console.WriteLine("");
+            Console.WriteLine("1. FizzBuzz");
+            Console.WriteLine("2. DeepDive");
+            Console.WriteLine("3. DrownItDown");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("-------------------------------");
+            Console.Write("Selected number: ");
+        }
+
+
     }
 }
