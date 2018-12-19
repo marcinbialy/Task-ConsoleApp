9002283f35ef708eb47d754fb32b365fc42cde5e
Marcin <bialkowski.marcin91@gmail.com>
2018-12-19 19:03:58 +0100

    Add DrownItDown

--- Task - ConsoleApp/Program.cs
+++ Task - ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Task___ConsoleApp
 {
@@ -19,8 +20,8 @@ namespace Task___ConsoleApp
             
             //MAIN PANEL
             MainPanel();
-
-            do
+            
+            do 
             {
                 // user choise input
                 string choise = Console.ReadLine();
@@ -54,7 +55,16 @@ namespace Task___ConsoleApp
                         }
                         break;
                     case "3":
-                        // 3. DrownItDown
+                        Console.Write("Enter the folder level to create the file: ");
+                        input = Console.ReadLine();
+                        if (int.TryParse(input, out number) && number <= 5)
+                        {
+                            p.DrownItDown(number);
+                            Console.Write(selectMethod);
+                        }
+                        else { Console.WriteLine(error);
+                               Console.Write(backToMainMenu);
+                        }
                         break;
                     case "4":
                         Exit();
@@ -63,6 +73,7 @@ namespace Task___ConsoleApp
                         Console.WriteLine(error);
                         break;
                 }
+
             } while (true);
         }
 
@@ -128,6 +139,57 @@ namespace Task___ConsoleApp
             string pathToCreate = Path.Combine(path, yourGuid); // path to first folder with GUID name
 
             return pathToCreate;
+        }
+
+        //3. DrownItDown
+        public void DrownItDown(int number)
+        {
+            if (list.Count != 0)
+            {
+                if (list.Count >= number)
+                {
+                    for (int i = 0; i < list.Count(); i++)
+                    {
+                        if (number - 1 == i)
+                        {
+                            //Console.WriteLine(list[i]); // path to save file
+
+                            if (!File.Exists($"{list[i]}\\empty.txt"))
+                            {
+                                File.WriteAllText(Path.Combine(list[i], "empty.txt"), " "); // create empty file
+                                Console.WriteLine("Empty file created successfully on " + number + " level!");
+                            }
+                            else // file overwritten
+                            {
+                                FileOverwriting(list[i]);
+                            }
+                        }
+                    }
+                }
+                else Console.WriteLine("You do not have this level of folders!");
+            }
+            else Console.WriteLine("If you want to create a file, first create folders with --> 2. DeepDive function ");
+        }
+
+        // File Overwriting if exsist
+        public void FileOverwriting(string dirPath)
+        {
+            Console.WriteLine("The file already exists at this level!");
+            Console.Write("Do you want to overwrite the file? Press y - for yes or n - for no: ");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "y":
+                    File.WriteAllText(Path.Combine(dirPath, "empty.txt"), " ");
+                    Console.WriteLine("The file has been successfully overwritten!");
+                    break;
+                case "n":
+                    Console.WriteLine("Ok, you back to main menu.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input! You back to main menu.");
+                    break;
+            }
         }
 
         //4. Exit 
