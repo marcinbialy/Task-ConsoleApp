f2cd8755ba0318aa2149ba0a2275e8338825519c
Marcin <bialkowski.marcin91@gmail.com>
2018-12-19 14:27:13 +0100

    Initial commit

---
+++ Task - ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task___ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
